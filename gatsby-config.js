7e7312dcd7526992b04a52bd816a18cf2b75ead3
Joao Garcia <joao@ludopoli.com>
2020-03-01 23:56:38 +0000

    updated favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'JoaoGarcia',
+        short_name: 'JoaoGarcia',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
